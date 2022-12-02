86545e7e82406ceb6c35e3d373823a947df29ebe
tyleraharrison <tyleraharrison@gmail.com>
2022-12-01 20:06:03 -0600

    Make test for non-401 HTTP errors
    Signed-off-by: tyleraharrison <tyleraharrison@gmail.com>

---
+++ tests/vectra_tests/vectra_api_http_error_test.py
@@ -0,0 +1,29 @@
+"""Test Vectra HTTP error handling"""
+# pylint: disable=wrong-import-position, import-error, too-few-public-methods, no-self-use
+import sys
+import os
+from azure.identity import DefaultAzureCredential
+import requests
+sys.path.append('..') # Import parent directory
+from dotenv import load_dotenv
+from soclib.vectra import VectraClient
+
+load_dotenv()
+
+class TestVectraAPIHTTPError:
+    """ Class for testing Vectra HTTP error handling """
+
+    def test_vectra_api_reauth(self):
+        """ Test a 404 exception being raised """
+        base_url = os.getenv('BASE_URL')
+
+        # Do authentication
+        vectra_client = VectraClient(base_url, DefaultAzureCredential())
+        
+        # Do get_detection with fake stakeholder and detection ID
+        status_code = 200
+        try:
+            vectra_client.get_detection("bad_stakeholder", "1")
+        except requests.exceptions.HTTPError as err:
+            status_code = err.response.status_code
+        assert status_code == 404 # nosec B101
