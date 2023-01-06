640488b5d3570142f044d747ee3597751f4a4fc2
jaebryson <jbrysonjr03@gmail.com>
2023-01-06 17:01:48 -0600

    fixed vectra/__init__.py

--- vectra/__init__.py
+++ vectra/__init__.py
@@ -4,9 +4,12 @@
 from .vectraclient import VectraClient
 
 from .utility import (
+    get_hostnames,
+    get_src_ip,
     get_dst_ips,
-    get_dst_ports,
+    get_related_ips,
     get_dst_subnets,
+    get_dst_ports,
     get_target_domains,
     get_dns_responses,
     add_extra_fields
