6437f5b60d082bbfd8a5c6727cee0d8be32eaf0f
jaebryson <jbrysonjr03@gmail.com>
2023-01-17 20:38:57 -0600

    Added __all__ to __init__.py for soclib.vectra

--- vectra/__init__.py
+++ vectra/__init__.py
@@ -1,6 +1,19 @@
 # pylint: disable=line-too-long
 """Package for  the SOC Vectra API Python wrapper."""
 
+__all__ = [
+    "VectraClient",
+    "get_hostnames",
+    "get_src_ip",
+    "get_dst_ips",
+    "get_related_ips",
+    "get_dst_subnets",
+    "get_dst_ports",
+    "get_target_domains",
+    "get_dns_responses",
+    "add_extra_fields"
+]
+
 from .vectraclient import VectraClient
 
 from .utility import (
